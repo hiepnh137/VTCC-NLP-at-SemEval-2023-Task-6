f0eda4bee588ce091fe533d507b5e274cee6900b
HuuHiep <64054312+hiepnh137@users.noreply.github.com>
2023-05-04 13:55:44 +0700

    Update eval.sh

--- eval.sh
+++ eval.sh
@@ -1,3 +1,1 @@
-# python infer_new_graph.py  ../data/test.json ../data/test_infer.json results/entity_graph_multitask_concat_InLegalBERT2/0_0_model.pt
-# python infer_new_graph.py  ../data/test.json ../data/test_infer_contrastive.json results/entity_graph_multitask_concat_InLegalBERT2/0_0_model.pt
 python infer_new_graph.py  ../data/test.json ../data/dev_infer.json results/entity_graph_multitask_concat_InLegalBERT2/0_0_model.pt
